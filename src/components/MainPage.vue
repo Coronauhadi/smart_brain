fa22b07de239f3316a4c043aef4c8ef43a96cb66
Matvey <tod13yung@yandex.ru>
2020-04-05 16:21:20 +0400

    Continuing

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -14,7 +14,6 @@
     <div class="row">
       <div class="col">
         <div class="">
-          <span class=""> 'Самостоятельность - это способность купить сосиски не по акции.' (с) Адам Смит </span>
         </div>
       </div>
     </div>
